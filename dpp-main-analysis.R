6e397346a9da3a655adf15150b98b5816665b947
Gerber <bgerber@uic.edu>
2019-09-24 10:32:52 -0500

    directory varies by user for reading data files

--- dpp-main-analysis.R
+++ dpp-main-analysis.R
@@ -4,7 +4,7 @@
 # Bernice Man's DPP GDM Analysis (Prediction Model)
 #
 # Started 8/27/19
-# Revised 9/16/19
+# Revised 9/24/19
 #
 # Description:
 #
@@ -14,19 +14,32 @@
 library(sas7bdat)
 library(tidyverse)
 
+# Get user name from Sys.info()
+username <- Sys.info()[7]
+
+# List paths for data directory
+paths <- c(
+  "C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\"
+)
+
+names(paths) <- c("bgerber")
+
+# Set path based on user
+setwd(paths[username])
+
 # Read in selected data files 
 # Forms:  S03, S05, Q03
 # Nonforms: basedata.sas7bdat, lab.sas7bdat, events.sas7bdat
 
-S03 <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\s03.sas7bdat")
-S05 <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\s05.sas7bdat")
-Q03 <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\q03.sas7bdat")
+S03 <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\s03.sas7bdat")
+S05 <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\s05.sas7bdat")
+Q03 <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Form_Data\\Data\\q03.sas7bdat")
 
-basedata <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\basedata.sas7bdat")
-lab <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\lab.sas7bdat")
-events <- read.sas7bdat("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\events.sas7bdat")
+basedata <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\basedata.sas7bdat")
+lab <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\lab.sas7bdat")
+events <- read.sas7bdat("Unzipped\\Data\\DPP_Data_2008\\Non-Form_Data\\Data\\events.sas7bdat")
 
-activities <- read.csv("C:\\Users\\bgerber\\Box Sync\\DPP-DPPOS\\Reproduced Analysis\\data\\MAQactivities with Updated METs.csv")
+activities <- read.csv("MAQactivities with Updated METs.csv")
 
 # Select data of interest for S03, S05 (specify variables)
 # Use "RUN" visit for Q03 physical activity data and lab data
